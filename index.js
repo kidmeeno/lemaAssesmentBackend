662f7e47ca4961652402935ab4ce8bf1359c2e38
kidmeeno <arthurbosah@yahoo.com>
2025-07-24 08:03:21 +0100

    resolve cors error

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/users');
 
-app.use(cors());
-app.use(express.json());
 
+app.use(cors({
+  origin: 'https://lema-frontend-phi.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+app.use((req, res, next) => {
+  console.log('Incoming request origin:', req.headers.origin);
+  next();
+});
+
+app.use(express.json());
 app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 3000;
